42f25e48170f55d557babf6633364111e5f2c7cd
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-07-11 13:12:09 +0900

    axios 인스턴스 및 HTTP 메서드 함수 추가

---
+++ src/libs/api/axiosInstance.ts
@@ -0,0 +1,92 @@
+import axios from "axios";
+
+import { getCookie } from "@/utils";
+
+type RefreshResponse = {
+  accessToken: string;
+  expiresIn: number;
+  issuedAt: number;
+  refreshToken: string;
+};
+
+export const axiosInstance = axios.create({
+  baseURL: "/api",
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const accessToken = getCookie("accessToken");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+let isRefreshing = false;
+let refreshSubscribers: ((token: string) => void)[] = [];
+
+const onTokenRefreshed = (token: string) => {
+  refreshSubscribers.forEach((callback) => callback(token));
+  refreshSubscribers = [];
+};
+
+const addRefreshSubscriber = (callback: (token: string) => void) => {
+  refreshSubscribers.push(callback);
+};
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    console.log(error);
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refreshToken = getCookie("refreshToken");
+
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
+
+      if (isRefreshing) {
+        return new Promise((resolve) => {
+          addRefreshSubscriber((token: string) => {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            resolve(axiosInstance(originalRequest));
+          });
+        });
+      }
+
+      isRefreshing = true;
+
+      try {
+        const response: RefreshResponse = await axiosInstance.post(
+          "/auth/refresh",
+          {
+            refreshToken,
+          }
+        );
+
+        document.cookie = `accessToken=${response.accessToken}; path=/;`;
+        document.cookie = `refreshToken=${response.refreshToken}; path=/;`;
+
+        console.log(response);
+
+        onTokenRefreshed(response.accessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${response.accessToken}`;
+
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
